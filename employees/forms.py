4f6a96154d89dd4f600b1dbdea204a331c789e0c
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-11-29 02:40:05 -0300

    Css is added

--- employees/forms.py
+++ employees/forms.py
@@ -1,9 +1,11 @@
 from django import forms
+from department.models import Department
 
 class EmpleadosFormulario(forms.Form):
     first_name = forms.CharField(max_length=50, required=True)
     last_name =  forms.CharField(max_length=50)
     position =  forms.CharField(max_length=50)
+    # department = forms.ModelChoiceField(queryset=Department)
     
 
 class ChiefsForm(forms.Form):
