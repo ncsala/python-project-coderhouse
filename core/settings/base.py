1a8cfa37f627a4baebc9f53b5ccffd73a692479b
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-12-21 19:14:31 -0300

    Change BASE_DIR file access way

--- core/settings/base.py
+++ core/settings/base.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\nicoe\Desktop\LOCAL-python\pythonproject\python_project\templates'],
+        'DIRS': [BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/settings/local.py
+++ core/settings/local.py
@@ -20,13 +20,15 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_DIRS = [r'C:\Users\nicoe\Desktop\LOCAL-python\pythonproject\python_project\static']
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [r'C:\Users\nicoe\Desktop\LOCAL-python\pythonproject\python_project\static']
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = [r'C:\Users\nicoe\Desktop\LOCAL-python\pythonproject\python_project\media']
+MEDIA_ROOT = [BASE_DIR / 'media']
 
 # Configuraciones de email
 EMAIL_USE_TLS = True
